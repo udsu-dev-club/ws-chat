40affef95cde6cc80352a2d83c3fe53457b47b3b
Evgeniy Bastrykov <bastrykov@elewise.com>
2017-04-08 10:51:32 +0400

    Add login to frontend

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,35 +1,43 @@
 import { WsService } from './../ws.service';
 import { IMessage } from './chat.component.stub';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { Subscriber } from 'rxjs/Rx';
 
 @Component({
     selector: 'app-chat',
     templateUrl: './chat.component.html',
     styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
     private ws: WsService;
     private body: string;
     private messages: IMessage[] = [];
+    private sub: Subscriber<any>;
+
+    @ViewChild('username') username: ElementRef;
     
     constructor(ws: WsService) {
         this.ws = ws;
     }
     
     ngOnInit() {
-        this.ws.onmessage(msg => {
-            this.messages.push({
-                timestamp: (new Date()).toDateString(),
-                author: '@' + msg.slice(0, 16),
-                body: msg.slice(16),
-            });
+        this.sub = this.ws.response.subscribe(msg => {
+            // console.log(msg);
         });
     }
-    
-    send(event: KeyboardEvent) {
-        if (event.keyCode === 13) {
-            this.ws.send(JSON.stringify(this.body));
-            this.body = '';
-        }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
     }
+
+    login() {
+        this.ws.send('LOGIN', {username: this.username.nativeElement.value});
+    }
+    
+    // send(event: KeyboardEvent) {
+    //     if (event.keyCode === 13) {
+    //         this.ws.send(JSON.stringify(this.body));
+    //         this.body = '';
+    //     }
+    // }
 }
--- src/app/ws.service.ts
+++ src/app/ws.service.ts
@@ -1,27 +1,72 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class WsService {
-  private conn: WebSocket;
-  private queue: string[] = [];
-
-  constructor() {
-    this.conn = new WebSocket('ws://localhost:8080/ws');
-    this.conn.addEventListener('open', () => {
-      this.queue.forEach(msg => this.conn.send(msg));
-      this.queue = [];
-    });
-  }
-
-  send(msg: string) {
-    if (this.conn.readyState === WebSocket.OPEN) {
-      this.conn.send(msg);
-    } else {
-      this.queue.push(msg);
+    private _conn: WebSocket;
+    private _queue: string[] = [];
+    private _logined = false;
+    private _id = 1;
+    
+    public response: EventEmitter<any>;
+    
+    get logined(): boolean {
+        return this._logined;
     }
-  }
+    
+    constructor() {
+        this._conn = new WebSocket('ws://localhost:8080/ws');
+        this._conn.addEventListener('open', () => {
+            this._queue.forEach(msg => {
+                this._conn.send(msg)
+            });
+            this._queue = [];
+        });
+        this.response = new EventEmitter<any>();
+        this._conn.addEventListener('message', e => {
+            const data = JSON.parse(e.data);
+            this.response.emit(data);
+        });
+        this.response
+            .subscribe(msg => {
+                if (msg.cmd === 'LOGIN' && msg.id > 0) {
+                    this._logined = true;
+                }
+            })
+        ;
+        this.response
+            .subscribe(msg => {
+                if (msg.cmd === 'LOGOUT' && msg.id > 0 && !msg.error) {
+                    this._logined = false;
+                }
+            })
+        ;
+    }
+    
+    send(cmd: string, data: any): Promise<any> {
+        const req = {
+            id: this._id++,
+            cmd: cmd,
+            data: data
+        }
+        const res = new Promise<any>(resolve => {
+            const sub = this.response
+                .subscribe(msg => {
+                    if (msg.id === req.id) {
+                        sub.unsubscribe();
+                        resolve(msg);
+                    }
+                });
+        });
 
-  onmessage(cb: (string) => void) {
-    this.conn.addEventListener('message', e => cb(JSON.parse(e.data)));
-  }
+        const sreq = JSON.stringify(req);
+
+        if (this._conn.readyState === WebSocket.OPEN) {
+            this._conn.send(sreq);
+        } else {
+            this._queue.push(sreq);
+        }
+
+        return res;
+    }
 }
